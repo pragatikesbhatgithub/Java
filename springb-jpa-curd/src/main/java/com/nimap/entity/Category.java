0f3a5fa74fc779d7cc8e961165ecca77002773b0
Pragati Kesbhat <103934433+pragatikesbhatgithub@users.noreply.github.com>
2022-04-21 12:44:26 +0530

    SpringBoot CURD Operations

---
+++ springb-jpa-curd/src/main/java/com/nimap/entity/Category.java
@@ -0,0 +1,36 @@
+package com.nimap.entity;
+
+
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int cid;
+	private String name;
+//	@OneToMany(fetch = FetchType.EAGER)
+//	private List<Product> product;
+	
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
